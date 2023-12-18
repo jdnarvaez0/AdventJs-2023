a823c5e403c7e529047c3cb9fa78f70930bd05ec
Juan Narvaez <juand.ns07@gmail.com>
2023-12-18 09:48:27 -0300

    🌱 [UPDATE] challenge 04

--- src/code/04.ts
+++ src/code/04.ts
@@ -2,7 +2,7 @@ export const decode = (message: string): string => {
   const regex = /\(([^()]+)\)/g
 
   while (regex.test(message)) {
-    message = message.replace(regex, (_, match) =>
+    message = message.replace(regex, (_: string, match: string) =>
       match.split('').reverse().join('')
     )
   }
