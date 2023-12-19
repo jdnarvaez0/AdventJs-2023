3800b2c4fa2d076d9dd5a7ce4a08246095d00e3a
Juan Narvaez <juand.ns07@gmail.com>
2023-12-18 23:02:41 -0300

    🌱 [UPDATE] challenge 05

--- src/code/05.ts
+++ src/code/05.ts
@@ -1,23 +1,21 @@
-const road = 'S..|...|..'
-const time = 10 // unidades de tiempo
-
 export const cyberReindeer = (road: string, time: number): string[] => {
-  const moves: string[] = [road]
+  const roadArray: string[] = [road]
   let a: number = 0
   let b: string = '.'
 
   for (let i: number = 1; i < time; i++) {
     if (i === 5) road = road.replace(/\|/g, '*')
+
     const newRoad: string = road.replace(/S[\.\*]/, `${b}S`)
+
     if (newRoad !== road) {
       a++
       b = road[a]
     }
+
     road = newRoad
-    moves.push(road)
+    roadArray.push(road)
   }
 
-  return moves
+  return roadArray
 }
-
-console.log(cyberReindeer(road, time))
